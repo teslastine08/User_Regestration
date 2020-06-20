a6d04e1e374f07832cf4661521dc83b0eb83d917
teslastine08 <teslastine08@gmail.com>
2020-06-20 11:54:09 +0530

    UC1: First name

--- User_Registration.sh
+++ User_Registration.sh
@@ -1,3 +1,17 @@
 ################# User Regestartion ###################
 
 echo "Welcome to User Regestration"
+
+firstname () {
+read -p "enter first name: " F
+
+if [[ $F =~ ^[A-Z]{1}([a-z]{2,}) ]]
+
+    then
+        echo "User_Name $F is valid."
+    else
+        echo "User_Name $F is invalid."
+ fi
+}
+
+firstname
