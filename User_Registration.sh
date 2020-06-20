a0312a311ac176e95390ba643c888c3f5d6d346c
teslastine08 <teslastine08@gmail.com>
2020-06-20 11:57:30 +0530

    UC2: Last name

--- User_Registration.sh
+++ User_Registration.sh
@@ -15,3 +15,17 @@ if [[ $F =~ ^[A-Z]{1}([a-z]{2,}) ]]
 }
 
 firstname
+
+lastname () {
+read -p "enter first name: " F
+
+if [[ $F =~ ^[A-Z]{1}([a-z]{2,}) ]]
+
+    then
+        echo "User_Name $F is valid."
+    else
+        echo "User_Name $F is invalid."
+ fi
+}
+
+lastname
